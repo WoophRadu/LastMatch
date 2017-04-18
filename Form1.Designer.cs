7e4e8a0a2c5723cebcce8fffaa6a023e996c7603
Wooph Radu <radu.wooph@gmail.com>
2017-04-18 18:14:56 +0300

    Made the window start centered on the screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -226,6 +226,7 @@
             this.HelpButton = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LastMatch";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
