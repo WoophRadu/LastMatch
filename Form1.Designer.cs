c0ecf59a94af101208703bea6ca408dafa4678f3
Wooph Radu <radu.wooph@gmail.com>
2017-04-18 18:11:57 +0300

    Moved and resized things

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,8 +33,6 @@
             this.buttonLoadMatch = new System.Windows.Forms.Button();
             this.timerButtonLoadMatch = new System.Windows.Forms.Timer(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.labelLoadMatch = new System.Windows.Forms.Label();
-            this.textBoxLoadMatch = new System.Windows.Forms.TextBox();
             this.Played = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.Player = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Kills = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,6 +40,8 @@
             this.Deaths = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MVPs = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Score = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelLoadMatch = new System.Windows.Forms.Label();
+            this.textBoxLoadMatch = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +49,7 @@
             // 
             this.labelStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.labelStatus.AutoSize = true;
-            this.labelStatus.Location = new System.Drawing.Point(12, 382);
+            this.labelStatus.Location = new System.Drawing.Point(12, 290);
             this.labelStatus.Name = "labelStatus";
             this.labelStatus.Size = new System.Drawing.Size(24, 13);
             this.labelStatus.TabIndex = 0;
@@ -57,7 +57,7 @@
             // 
             // buttonLoadMatch
             // 
-            this.buttonLoadMatch.Location = new System.Drawing.Point(272, 5);
+            this.buttonLoadMatch.Location = new System.Drawing.Point(268, 26);
             this.buttonLoadMatch.Name = "buttonLoadMatch";
             this.buttonLoadMatch.Size = new System.Drawing.Size(75, 23);
             this.buttonLoadMatch.TabIndex = 1;
@@ -84,29 +84,11 @@
             this.Deaths,
             this.MVPs,
             this.Score});
-            this.dataGridView1.Location = new System.Drawing.Point(36, 32);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 55);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(794, 225);
+            this.dataGridView1.Size = new System.Drawing.Size(743, 225);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // labelLoadMatch
-            // 
-            this.labelLoadMatch.AutoSize = true;
-            this.labelLoadMatch.Location = new System.Drawing.Point(12, 9);
-            this.labelLoadMatch.Name = "labelLoadMatch";
-            this.labelLoadMatch.Size = new System.Drawing.Size(54, 13);
-            this.labelLoadMatch.TabIndex = 3;
-            this.labelLoadMatch.Text = "Steam ID:";
-            // 
-            // textBoxLoadMatch
-            // 
-            this.textBoxLoadMatch.Enabled = false;
-            this.textBoxLoadMatch.Location = new System.Drawing.Point(72, 6);
-            this.textBoxLoadMatch.Name = "textBoxLoadMatch";
-            this.textBoxLoadMatch.Size = new System.Drawing.Size(194, 20);
-            this.textBoxLoadMatch.TabIndex = 4;
-            this.textBoxLoadMatch.Text = "Hardcoded for Alive eSports RO";
             // 
             // Played
             // 
@@ -157,11 +139,28 @@
             this.Score.ReadOnly = true;
             this.Score.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
+            // labelLoadMatch
+            // 
+            this.labelLoadMatch.AutoSize = true;
+            this.labelLoadMatch.Location = new System.Drawing.Point(12, 30);
+            this.labelLoadMatch.Name = "labelLoadMatch";
+            this.labelLoadMatch.Size = new System.Drawing.Size(54, 13);
+            this.labelLoadMatch.TabIndex = 3;
+            this.labelLoadMatch.Text = "Steam ID:";
+            // 
+            // textBoxLoadMatch
+            // 
+            this.textBoxLoadMatch.Enabled = false;
+            this.textBoxLoadMatch.Location = new System.Drawing.Point(68, 27);
+            this.textBoxLoadMatch.Name = "textBoxLoadMatch";
+            this.textBoxLoadMatch.Size = new System.Drawing.Size(194, 20);
+            this.textBoxLoadMatch.TabIndex = 4;
+            this.textBoxLoadMatch.Text = "Hardcoded for Alive eSports RO";
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(856, 404);
+            this.ClientSize = new System.Drawing.Size(856, 312);
             this.Controls.Add(this.textBoxLoadMatch);
             this.Controls.Add(this.labelLoadMatch);
             this.Controls.Add(this.dataGridView1);
